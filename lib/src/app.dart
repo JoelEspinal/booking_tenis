5e2bd42390d3d902a7e2b7c4213a2bc56654bf7c
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-01 20:16:25 -0400

    🎉 Created project

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TENIS',
+      home: Container(
+        color: Colors.blue[300],
+      ),
+    );
+  }
+}
