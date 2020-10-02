35f8c489b6501b346d945812aec675490c5fb2c7
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-01 22:12:00 -0400

    🚧 Adds home page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,9 @@
+import 'package:booking_tenis/src/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TENIS',
-      home: Container(
-        color: Colors.blue[300],
-      ),
-    );
+    return MaterialApp(title: 'TENIS', home: HomePage());
   }
 }
