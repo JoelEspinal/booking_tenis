d66de3c291ec383069e4b162a2db4a75002a011e
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-03 20:09:11 -0400

    🚧 Add reservation table

---
+++ lib/src/data/tables/reservation.dart
@@ -0,0 +1,8 @@
+import 'package:moor/moor.dart';
+
+class Reservation extends Table {
+  IntColumn get id => integer().autoIncrement().nullable()();
+  TextColumn get court => text()();
+  DateTimeColumn get form => dateTime()();
+  DateTimeColumn get to => dateTime()();
+}
