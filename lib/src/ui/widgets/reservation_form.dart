52416cc115fcabca79481443389001b6d436922c
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-04 17:21:34 -0400

    ✨ Add reservation via method

--- lib/src/ui/widgets/reservation_form.dart
+++ lib/src/ui/widgets/reservation_form.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../../data/daos/reservation_dao.dart';
 
 class ReservationForm extends StatelessWidget {
   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context) {
+    final dao = Provider.of<ReservationDao>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('RESERVAR'),
@@ -16,8 +21,19 @@ class ReservationForm extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.check),
-            onPressed: () {
-              Navigator.of(context).pop();
+            onPressed: () async {
+              var _result = await _addReservation(dao);
+
+              Fluttertoast.showToast(
+                  msg: (_result > -1)
+                      ? 'Reservada agregada'
+                      : 'Conflicto al reservar',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.blue,
+                  textColor: (_result > -1) ? Colors.white : Colors.red[600],
+                  fontSize: 16.0);
             },
           ),
         ],
@@ -85,5 +101,18 @@ class ReservationForm extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<int> _addReservation(dao) async {
+    if (_fbKey.currentState.validate()) {
+      var _owner = _fbKey.currentState.fields['name'].currentState.value;
+      var _court = _fbKey.currentState.fields['court'].currentState.value;
+      var _from = _fbKey.currentState.fields['from'].currentState.value;
+      var _to = _fbKey.currentState.fields['to'].currentState.value;
+
+      return await dao.addReservation(
+          court: _court, reservationOwner: _owner, form: _from, to: _to);
+    }
+    return 0;
   }
 }
