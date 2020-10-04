d6efe116f1fdaee0649f2b83b7641e53662c508d
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-03 20:23:05 -0400

    🔧 Adds provider to run app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'src/app.dart';
+import 'src/data/daos/reservation_dao.dart';
+import 'src/data/moor_database.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => AppDatabase(),
+        ),
+        Provider(
+          create: (context) => ReservationDao(AppDatabase()),
+        ),
+      ],
+      child: App(),
+    ),
+  );
 }
