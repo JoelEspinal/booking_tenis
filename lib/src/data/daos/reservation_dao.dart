b5d7f1e500db056d8db0aecf7ff0ae92b44e0464
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-04 10:27:09 -0400

    ✨ Validate before insertion

--- lib/src/data/daos/reservation_dao.dart
+++ lib/src/data/daos/reservation_dao.dart
@@ -11,14 +11,36 @@ class ReservationDao extends DatabaseAccessor<AppDatabase>
   final AppDatabase db;
   ReservationDao(this.db) : super(db);
 
-  Future<int> addReservation(ReservationData reservation) {
-    final _entry = ReservationCompanion(
-        court: Value(reservation.court),
-        form: Value(reservation.form),
-        to: Value(reservation.to));
+  Future<int> addReservation(ReservationData reservation) async {
+    final validInsert = await isValidInsertion(reservation);
+    if (validInsert) {
+      final _entry = ReservationCompanion(
+          court: Value(reservation.court),
+          form: Value(reservation.form),
+          to: Value(reservation.to));
 
-    return into($ReservationTable(db)).insert(_entry);
+      return into($ReservationTable(db)).insert(_entry);
+    } else {
+      return Future.value(-1);
+    }
   }
+
+  Future<bool> isValidInsertion(ReservationData reservation) async {
+    final reservations = await getAllRservations();
+    var invalidReservation = reservations
+        .where((r) =>
+            r.court == reservation.court &&
+            (r.form.isBefore(reservation.form) ||
+                r.form.isAtSameMomentAs(reservation.form)) &&
+            (r.to.isAfter(reservation.to) ||
+                r.to.isAtSameMomentAs(reservation.to)))
+        .toList();
+
+    return Future.value(invalidReservation.length <= 0);
+  }
+
+  Future<List<ReservationData>> getAllRservations() =>
+      select(reservation).get();
 
   Stream<List<ReservationData>> watchAllReservations() {
     return (select(reservation)
