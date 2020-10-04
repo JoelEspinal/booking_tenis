ffcc97d2b3c5e87448782efb06c9dea3a18ec99f
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-03 20:10:50 -0400

    🚧 Add reservation dao

---
+++ lib/src/data/daos/reservation_dao.dart
@@ -0,0 +1,30 @@
+import 'package:moor/moor.dart';
+
+import '../moor_database.dart';
+import '../tables/reservation.dart';
+
+part 'reservation_dao.g.dart';
+
+@UseDao(tables: [Reservation])
+class ReservationDao extends DatabaseAccessor<AppDatabase>
+    with _$ReservationDaoMixin {
+  final AppDatabase db;
+  ReservationDao(this.db) : super(db);
+
+  Future<int> addReservation(ReservationData reservation) {
+    final _entry = ReservationCompanion(
+        court: Value(reservation.court),
+        form: Value(reservation.form),
+        to: Value(reservation.to));
+
+    return into($ReservationTable(db)).insert(_entry);
+  }
+
+  Stream<List<ReservationData>> watchAllTasks() {
+    return (select(reservation)
+          ..orderBy([
+            (r) => OrderingTerm(expression: r.form, mode: OrderingMode.desc),
+          ]))
+        .watch();
+  }
+}
