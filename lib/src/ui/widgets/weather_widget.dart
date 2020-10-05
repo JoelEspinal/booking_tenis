0b55a60da552fdafc757f6c700a05b378451a32f
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2020-10-05 08:21:31 -0400

    ✨ Add wheather widget

---
+++ lib/src/ui/widgets/weather_widget.dart
@@ -0,0 +1,44 @@
+import 'package:booking_tenis/src/remote/rain_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class WheatherWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Provider.of<RainService>(context, listen: false)
+            .fetchRainProbabilities(),
+        builder: (context, data) {
+          if (data.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (data.error != null) {
+              return Center(child: Text(':/'));
+            } else {
+              return Consumer<RainService>(builder: (ctx, provabilites, child) {
+                return Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: ListTile(
+                      subtitle: Text(
+                        'Probabilidad de lluvia',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      leading: CircleAvatar(
+                        backgroundColor:
+                            Theme.of(context).scaffoldBackgroundColor,
+                        child: Text(
+                          '',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+                      trailing: Text(
+                        '${provabilites.rainPercent.toStringAsPrecision(2)} %',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ));
+              });
+            }
+          }
+        });
+  }
+}
